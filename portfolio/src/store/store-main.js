07d50bf2d9a58c2f84beff5fe9b768ca87ae9539
Mallow <mencheliu@gmail.com>
2026-01-31 15:50:50 +0800

    adjust isDark

--- portfolio/src/store/store-main.js
+++ portfolio/src/store/store-main.js
@@ -4,7 +4,12 @@ import { useDark } from "@vueuse/core";
 //setup 語法
 export const useStore = defineStore("main", () => {
   // ref()
-  const isDark = useDark();
+  const isDark = useDark({
+    selector: "html",
+    attribute: "class",
+    valueDark: "dark",
+    valueLight: "",
+  });
 
   //computed
   //function()
